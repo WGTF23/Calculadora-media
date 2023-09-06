21a490bbe90a020c2a6996c002408b6216bbe681
WGTF23 <giosagit@gmail.com>
2023-09-05 21:10:03 -0300

    Envio de arquivos

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Quiero Kekote de la Kataaaa 😋😊😉") //comando de saida de informação
+
+/*
+Grandote e formoso, gosto bastaaaaannnnteeeeee */
+console.log("Bem vindo🍑")
+
+alert("a media e:")
+alert((8+7+9+2)/4)
+
+console.log((8+7+9+2)/4)
